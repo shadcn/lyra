bc98ea5b34ab48d334a62bda4f4e27bfa2aa5678
Arshad Chummun <arshad@chummun.com>
2013-08-21 17:27:40 +0400

    Updated brand and menu css for multiple viewports

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -3,5 +3,20 @@
  * JS for Radix Starter.
  */
 (function ($) {
-  // code here
+  $(document).ready(function() {
+    // Animation for search form
+    $('.search-form .form-text').focus(function() {
+      $(this).animate({
+        width: 200
+      }, 500);
+    });
+
+    $('.search-form .form-text').blur(function() {
+      if (!$(this).val()) {
+        $(this).animate({
+          width: 100
+        }, 500);
+      }
+    });
+  });
 })(jQuery);
